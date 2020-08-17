b201bc15be08580258924bb9886b5d85c7b78da5
RennanD <rplaykl@gmail.com>
2020-08-17 14:04:32 -0300

    feat: Build

--- src/database/mongoConnect.ts
+++ src/database/mongoConnect.ts
@@ -1,6 +1,8 @@
 import { connect } from 'mongoose';
 
 const database = connect(`${process.env.MONGO_URL}`, {
+  user: process.env.MONGO_USER,
+  pass: process.env.MONGO_PASS,
   useFindAndModify: true,
   useNewUrlParser: true,
   useUnifiedTopology: true,
