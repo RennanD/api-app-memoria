6210d86e42add707464915101996b421a5dff05d
RennanD <rplaykl@gmail.com>
2020-08-17 14:17:11 -0300

    feat: Build

--- src/database/mongoConnect.ts
+++ src/database/mongoConnect.ts
@@ -1,12 +1,13 @@
 import { connect } from 'mongoose';
 
-const database = connect(`${process.env.MONGO_URL}`, {
-  user: process.env.MONGO_USER,
-  pass: process.env.MONGO_PASS,
-  useFindAndModify: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const database = connect(
+  `mongodb://app-memoria:app-memoria-test@localhost:27027/app-memoria`,
+  {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+);
 
 module.exports = database;
