a9219cfc549dd99d16f0c31f36bd9c0f97f2d343
RennanD <rplaykl@gmail.com>
2020-10-18 20:53:46 -0300

    feat: Finish reminders

--- src/database/mongoConnect.ts
+++ src/database/mongoConnect.ts
@@ -1,10 +1,13 @@
 import { connect } from 'mongoose';
 
-const database = connect('mongodb://localhost:27017/teste2', {
-  useFindAndModify: true,
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+const database = connect(
+  'mongodb+srv://rennan:rennan@cluster0.sh5jy.mongodb.net/app-memoria?retryWrites=true&w=majority',
+  {
+    useFindAndModify: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+);
 
 module.exports = database;
