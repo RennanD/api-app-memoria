5cfe91f0655e46729cc25d4f3a4389b46e9d9a96
RennanD <rplaykl@gmail.com>
2020-10-18 22:19:57 -0300

    feat: Finish reminders

--- src/modules/user/routes/notifications.routes.ts
+++ src/modules/user/routes/notifications.routes.ts
@@ -1,12 +1,27 @@
 import { Router } from 'express';
 
 import ensureAuthenticated from '../../../middlewares/ensureAuthenticate';
+import NotificationsToken from '../schemas/NotificationsToken';
 
 import CreateNotificationTokenService from '../services/CreateNotificationTokenService';
 
 const notificationsRouter = Router();
 
 notificationsRouter.use(ensureAuthenticated);
+
+notificationsRouter.get('/token/:my_token', async (request, response) => {
+  const { my_token } = request.params;
+
+  const token = await NotificationsToken.findOne({
+    token: my_token,
+  });
+
+  if (!token) {
+    return response.json({ hasToken: false });
+  }
+
+  return response.json({ hasToken: true });
+});
 
 notificationsRouter.post('/token', async (request, response) => {
   const createToken = new CreateNotificationTokenService();
