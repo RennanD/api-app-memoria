7943d253740d7c92c04c26de665e4e544276ea1b
RennanD <rplaykl@gmail.com>
2020-10-21 20:53:02 -0300

    feat: Finish reminders

--- src/app.ts
+++ src/app.ts
@@ -17,23 +17,7 @@ import userRoutes from './modules/user/routes';
 import adminRoutes from './modules/admin/routes';
 
 cron.schedule('*/4 20-21 * * *', async () => {
-  const message = {
-    to: 'ExponentPushToken[v_SRqyMrHuM9ejem04RU91]',
-    sound: 'default',
-    title: 'Notificação',
-    body: 'Ele passou daqui, fica ligago',
-    data: { data: 'goes here' },
-  };
-
-  await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Accept-encoding': 'gzip, deflate',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(message),
-  });
+  console.log('passou');
   await getReminders();
 });
 
