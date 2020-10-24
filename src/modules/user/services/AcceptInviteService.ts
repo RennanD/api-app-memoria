23d5cc9cba6390f1415ebf3b083698467979e561
RennanD <rplaykl@gmail.com>
2020-10-23 22:46:09 -0300

    feat: Finish notifications/create date in accept invite

--- src/modules/user/services/AcceptInviteService.ts
+++ src/modules/user/services/AcceptInviteService.ts
@@ -1,12 +1,14 @@
 import { getRepository } from 'typeorm';
+import { format } from 'date-fns';
 
 import Contact from '../models/Contact';
 import Account from '../models/Account';
+import ImportantDate from '../models/ImportantDate';
 
 import AppError from '../../../errors/AppError';
 
-import CreateImportantDateService from './CreateImportantDateService';
 import CreateNotificationService from './CreateNotificationService';
+import CreateRemindersService from './CreateRemindersService';
 
 interface Request {
   owner_id: string;
@@ -17,9 +19,10 @@ class AcceptInviteService {
   public async execute({ owner_id, guest_id }: Request): Promise<Contact> {
     const accountRespository = getRepository(Account);
     const contactRespository = getRepository(Contact);
+    const dateRepository = getRepository(ImportantDate);
 
-    const createImportantDate = new CreateImportantDateService();
     const createNotification = new CreateNotificationService();
+    const createReminders = new CreateRemindersService();
 
     const ownerAccount = await accountRespository.findOne({
       where: { user_id: owner_id },
@@ -65,22 +68,60 @@ class AcceptInviteService {
     await contactRespository.save(owner);
     await contactRespository.save(guest);
 
-    const guestDate = await createImportantDate.execute({
+    const ownerDate = dateRepository.create({
       user_id: ownerAccount.user.id,
       contact_id: guestAccount.user.id,
       date: guestAccount.user.birthday,
       description: `Aniversário de ${guestAccount.user.name}`,
     });
 
-    await createImportantDate.execute({
+    const guestDate = dateRepository.create({
       user_id: guestAccount.user.id,
       contact_id: ownerAccount.user.id,
       date: ownerAccount.user.birthday,
       description: `Aniversário de ${ownerAccount.user.name}`,
     });
 
+    await dateRepository.save(ownerDate);
+    await dateRepository.save(guestDate);
+
+    const stringDateGuest = format(guestDate.date, "MM'-'dd");
+    const getMonthGuest = guestDate.date.getMonth() + 1;
+    const getDayGuest = guestDate.date.getDate();
+
+    const stringDateOwner = format(ownerDate.date, "MM'-'dd");
+    const getMonthOwner = ownerDate.date.getMonth() + 1;
+    const getDayOwner = ownerDate.date.getDate();
+
+    const reminders = [
+      {
+        user_id: ownerAccount.user.id,
+        important_date_id: guestDate.id,
+        title: `Aniversário de ${guestAccount.user.name}`,
+        date: `10 ${getDayGuest} ${getMonthGuest} *`,
+        reminderDate: guestDate.date,
+        parsed_date: stringDateGuest,
+        notification_message: `Hoje é aniversário de ${guestAccount.user.name}, lhe deseje feliz aniversário! :)`,
+        active: true,
+      },
+      {
+        user_id: ownerAccount.user.id,
+        important_date_id: ownerDate.id,
+        title: `Aniversário de ${ownerAccount.user.name}`,
+        date: `10 ${getDayOwner} ${getMonthOwner} *`,
+        reminderDate: ownerDate.date,
+        parsed_date: stringDateOwner,
+        notification_message: `Hoje é aniversário de ${ownerAccount.user.name}, lhe deseje feliz aniversário! :)`,
+        active: true,
+      },
+    ];
+
+    await createReminders.execute({
+      reminders,
+    });
+
     await createNotification.execute({
-      description: `${guestAccount.user.name}`,
+      description: `${guestAccount.user.name} aceitou seu convite.`,
       important_date_id: guestDate.id,
       user_id: ownerAccount.user.id,
     });
