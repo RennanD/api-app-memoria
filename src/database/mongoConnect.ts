c444b0a3c5a700a100b28079016734a4e6412e38
RennanD <rplaykl@gmail.com>
2020-11-16 17:15:08 -0300

    api - change env

--- src/database/mongoConnect.ts
+++ src/database/mongoConnect.ts
@@ -1,7 +1,8 @@
 import { connect } from 'mongoose';
 
 const database = connect(
-  'mongodb+srv://rennan:rennan@cluster0.sh5jy.mongodb.net/app-memoria?retryWrites=true&w=majority',
+  // 'mongodb+srv://rennan:rennan@cluster0.sh5jy.mongodb.net/app-memoria?retryWrites=true&w=majority',
+  'mongodb://localhost:27017/app-memoria',
   {
     useFindAndModify: true,
     useNewUrlParser: true,
